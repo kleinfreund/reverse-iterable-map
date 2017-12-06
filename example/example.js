ebc85740970e1719e49935190aaec57d67c5970f
kleinfreund <phrudloff@gmail.com>
2017-12-06 15:01:02 +0100

    Fix example

--- example/example.js
+++ example/example.js
@@ -7,12 +7,20 @@ map
   .push('key3', '3');
 
 for (const [key, value] of map) {
-  console.log('[', key, value, ']');
+  console.log(key, ':', value);
 }
 
 let reverse = true;
 for (const [key, value] of map.entries(reverse)) {
-  console.log('[', key, v, ']');
+  console.log(key, ':', value);
 }
 
 console.log(map.size);
+
+map.delete('key2');
+
+console.log(map.size);
+
+for (const value of map.values()) {
+  console.log(value);
+}
