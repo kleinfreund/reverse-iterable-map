a8989bb91535d3d6c8d8a757c067129e4b69efe4
kleinfreund <phrudloff@gmail.com>
2017-12-07 10:48:45 +0100

    Expand tests

--- example/example.js
+++ example/example.js
@@ -1,26 +1,58 @@
 import { LinkedMap } from '../src/linked-map.js';
 
-const map = new LinkedMap();
-map
-  .push('key1', '1')
-  .push('key2', '2')
-  .push('key3', '3');
+const body = document.querySelector('body');
 
-for (const [key, value] of map) {
-  console.log(key, ':', value);
+function print(...args) {
+  body.insertAdjacentHTML('beforeend', `<div>${args.join(' ')}</div>`);
 }
 
-let reverse = true;
-for (const [key, value] of map.entries(reverse)) {
-  console.log(key, ':', value);
+function runExample() {
+  const map = new LinkedMap();
+  map
+    .push('key1', '1')
+    .push('key2', '2')
+    .push('key3', '3');
+
+  print('[key, value] of map');
+  for (const [key, value] of map) {
+    print(key, ':', value);
+  }
+
+  print('[key, value] of map.entries(true)');
+  for (const [key, value] of map.entries(true)) {
+    print(key, ':', value);
+  }
+
+  print('key of map.keys()');
+  for (const key of map.keys()) {
+    print(key);
+  }
+
+  print('value of map.values()');
+  for (const value of map.values()) {
+    print(value);
+  }
+
+  print('[key, value] of map.reverse()');
+  for (const [key, value] of map.reverse()) {
+    print(key, ':', value);
+  }
+
+  print('[...map]:');
+  print([...map]);
+
+  print('[...map.reverse()]:');
+  print([...map.reverse()]);
+
+  print('Current size:', map.size);
+  print('map.delete("key2")');
+  map.delete('key2');
+  print('Current size:', map.size);
+
+  print('value of map.values()');
+  for (const value of map.values()) {
+    print(value);
+  }
 }
 
-console.log(map.size);
-
-map.delete('key2');
-
-console.log(map.size);
-
-for (const value of map.values()) {
-  console.log(value);
-}
+runExample();
